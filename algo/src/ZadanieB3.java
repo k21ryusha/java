88734d242eb3f5ea552024ce9d36ae6183b97e14
k21ryusha <andropov2005@inbox.ru>
2024-10-04 16:35:51 +0300

    finish first lab!)

--- algo/src/ZadanieB3.java
+++ algo/src/ZadanieB3.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class Zadanie_B_3{
+
+public class ZadanieB3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String input = in.next();
@@ -13,7 +14,7 @@ public class Zadanie_B_3{
             if (Character.isLowerCase(currentChar)) {
                 System.out.print(currentChar);
             } else if (Character.isUpperCase(currentChar)) {
-                System.out.print(" "+currentChar);
+                System.out.print(" " + currentChar);
             }
         }
     }
--- algo/src/Zadanie_osnovnoe.java
+++
@@ -1,3 +0,0 @@
-public class Zadanie_osnovnoe {
-/// mama///
-}
